72ec609927d51717abce92e130541d843a461969
ImanolPerez <140122370+ImanolPerezz@users.noreply.github.com>
2023-08-22 20:38:34 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,14 @@
+var nombre="Imanol"
+var apellido="Perez"
+console.log(nombre + apellido)
+
+function suma(numero1 , numero2){
+    var suma=numero1 + numero2
+    console.log("El resultado de la suma sera", suma)
+}
+
+suma(5,9)
+suma(-2,-5)
+suma(91293,18238123)
+suma(999,232)
+suma(10,20)
